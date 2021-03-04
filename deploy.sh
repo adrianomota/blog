ba6a8cfdfd385c3d0549c3c6cac3833b7e075738
Adriano Mota <adrianowsh@hotmail.com>
2021-03-03 23:50:11 -0300

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "start gigalixir Deploy........."
-git remote add gigalixir https://git.gigalixir.com/blogelxpro.git/
+git remote add gigalixir https://git.gigalixir.com/blogelxpro.git
 git push gigalixir master
 echo "finish gigalixir Deploy........."
