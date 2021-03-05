31778913254c24e3b5ff09e9b0e8f9882b896ce8
Adriano Mota <adrianowsh@hotmail.com>
2021-03-05 20:23:07 -0300

    Travis CI CD Gigalixir

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "starting deploy process"
+
+git remote add gigalixir https://adrianowshgmail.com:0b0630f7-3bfc-4bd5-aa31-84ce941e5c900@git.gigalixir.com/blogelxpro.git
+
+BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR"
+echo "------------------------------------"
+echo "BRANCH=$BRANCH"
+
+if [ "$BRANCH" == "master" ]; then
+  echo "Pushing HEAD to master branch on Gigalixir."
+  git push -f gigalixir HEAD:refs/heads/master
+  echo "Deploy completed."
+fi
+
+echo "Exiting."
