aac5a12d687491224a9a187dff8721f523b6903e
Adriano Mota <adrianowsh@hotmail.com>
2021-03-04 22:22:32 -0300

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
-echo "starting deploy process"
-git remote add gigalixir https://adrianowshgmail.com:0b0630f7-3bfc-4bd5-aa31-84ce941e5c90@git.gigalixir.com/blogelxpro.git
+#!/usr/bin/env bash
+
+git remote add gigalixir https://$GIGALIXIR_EMAIL:$GIGALIXIR_API_KEY@git.gigalixir.com/$GIGALIXIR_APP_NAME.git
+
 BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR"
 echo "------------------------------------"
 echo "BRANCH=$BRANCH"
+
 if [ "$BRANCH" == "master" ]; then
   echo "Pushing HEAD to master branch on Gigalixir."
-  git push gigalixir HEAD:master
+  git push gigalixir HEAD:master --verbose
   echo "Deploy completed."
 fi
+
 echo "Exiting."
