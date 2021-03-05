87f96da79f5c32dea995746a0416676720be50be
Adriano Mota <adrianowsh@hotmail.com>
2021-03-05 20:34:48 -0300

    fixed travis config

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "starting deploy process"
 
-git remote add gigalixir https://adrianowshgmail.com:0b0630f7-3bfc-4bd5-aa31-84ce941e5c900@git.gigalixir.com/blogelxpro.git
+git remote add gigalixir https://adrianowshgmail.com:0b0630f7-3bfc-4bd5-aa31-84ce941e5c90@git.gigalixir.com/blogelxpro.git
 
 BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR"
