c194e60e08bd748b6bb417453326450ea1c1d0ea
Adriano Mota <adrianowsh@hotmail.com>
2021-03-09 12:16:42 -0300

    finalização do crud

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -19,8 +19,70 @@ defmodule BlogWeb.PostController do
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"title" => title, "description" => decription}) do
-    posts = Repo.all(Post)
-    render(conn, "index.html", posts: posts)
+  def edit(conn, %{"id" => id}) do
+    post_exists? = Repo.get(Post, id)
+
+    if post_exists? != nil do
+      changeset = Post.changeset(post_exists?)
+
+      conn
+      |> render("edit.html", post: post_exists?, changeset: changeset)
+    else
+      conn
+      |> put_flash(:info, "Post não existe")
+      |> render("index.html", posts: Repo.all(Post))
+    end
+  end
+
+  def create(conn, %{"post" => post}) do
+    new_post = Post.changeset(%Post{}, post)
+
+    case Repo.insert(new_post) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post criado com sucesso")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def update(conn, %{"id" => id, "post" => post}) do
+    post_exists? = Repo.get(Post, id)
+
+    if post_exists? != nil do
+      changeset = Post.changeset(post_exists?, post)
+
+      case Repo.update(changeset) do
+        {:ok, post} ->
+          conn
+          |> put_flash(:info, "Post atualizado com sucesso")
+          |> redirect(to: Routes.post_path(conn, :show, post))
+
+        {:error, changeset} ->
+          render(conn, "new.html", changeset: changeset)
+      end
+    else
+      conn
+      |> put_flash(:info, "Impossível atualizar o Post")
+      |> redirect(to: Routes.post_path(conn, :show, post))
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    post = Repo.get(Post, id)
+
+    if post != nil do
+      Repo.delete!(post)
+
+      conn
+      |> put_flash(:info, "Post excluído com sucesso")
+      |> redirect(to: Routes.post_path(conn, :index, Repo.all(Post)))
+    else
+      conn
+      |> put_flash(:error, "Post não existe!")
+      |> render("index.html", posts: Repo.all(Post))
+    end
   end
 end
